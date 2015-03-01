aceabf16272da26e00f6a440526bee2d17e9fa36
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-01 19:00:40 +0300

    Count change.

--- src/sicp/chapter1.clj
+++ src/sicp/chapter1.clj
@@ -30,3 +30,57 @@
                        guess
                        (recur (improve guess))))]
     (sqrt-iter 1.0)))
+
+"Recursion and iteration. Tree recursion."
+
+"Factorial."
+
+(defn factorial
+  [n]
+  (cond (<= n 1) 1
+        :else (* n (factorial (- n 1)))))
+
+(defn factorial-iteration
+  [n]
+  (letfn [(fact-iter
+            [product count]
+            (cond (> count n) product
+                  :else (recur (* product count) (inc count))))]
+    (fact-iter 1 1)))
+
+"Fibonacci."
+
+(defn fibonacci
+  [n]
+  (cond (<= n 0) 0
+        (= n 1) 1
+        :else (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
+
+(defn fibonacci-iteration
+  [n]
+  (letfn [(fib-iter
+            [a b count]
+            (cond (>= count n) a
+                  :else (recur (+ a b) a (inc count))))]
+    (fib-iter 0 1 0)))
+
+"Count change."
+
+(defn first-denomination
+  [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+(defn cc
+  [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc amount (- kinds-of-coins 1))
+                 (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins))))
+
+(defn count-change
+  [amount]
+  (cc amount 5))
--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -7,4 +7,9 @@
   [& args]
   (println "Hello, World!")
   (prn (chapter1/sqrt 9))
-  (prn (chapter1/new-if (= 1 1) 0 5)))
+  (prn (chapter1/new-if (= 1 1) 0 5))
+  (prn (chapter1/factorial 7))
+  (prn (chapter1/factorial-iteration 7))
+  (prn (chapter1/fibonacci 10))
+  (prn (chapter1/fibonacci-iteration 10))
+  (prn (chapter1/count-change 100)))
